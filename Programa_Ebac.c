b7dd46cf7287c6149a693319f7afbd88ed9bc193
Hebert Wallace <125490073+hwallydev@users.noreply.github.com>
2024-05-26 18:39:05 -0300

    Opção de Saída
    Incluso no sistema a opção de saída para o usuário

--- Programa_Ebac.c
+++ Programa_Ebac.c
@@ -135,8 +135,10 @@ int main(){
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n ");
 		printf("\t2 - Consultar nomes\n ");
-		printf("\t3 - Deletar nomes\n\n "); 
+		printf("\t3 - Deletar nomes\n ");
+		printf("\t4 - Sair do Sistema: \n\n");
 		printf("Digite a opção: "); //Fim do menu
+		
 		
 		scanf("%d", &opcao); //Armazenando a escolha do usuário
 		
@@ -154,6 +156,11 @@ int main(){
 			
 			case 3:
 				deletar();
+				break;
+			
+			case 4:
+				printf("Obrigado por utilizar o sistema\n\n");
+				return 0;
 				break;
 				
 			default:
